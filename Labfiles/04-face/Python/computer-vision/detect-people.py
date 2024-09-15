115f55cef25b868b39087990b8f6c410057194cc
saikrmet <saikrmet@gmail.com>
2024-09-15 17:15:25 -0400

    Finished face using AI Vision

--- Labfiles/04-face/Python/computer-vision/detect-people.py
+++ Labfiles/04-face/Python/computer-vision/detect-people.py
@@ -8,6 +8,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # Import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+
+
 
 
 
@@ -26,22 +31,47 @@ def main():
             image_file = sys.argv[1]
 
         # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(endpoint=ai_endpoint, credential=AzureKeyCredential(ai_key))
+        with open(image_file, "rb") as f:
+            image_data = f.read()
 
         
         # Analyze image
-        AnalyzeImage(image_file, cv_client)
+        AnalyzeImage(image_file, image_data, cv_client)
 
     except Exception as ex:
         print(ex)
 
 
-def AnalyzeImage(image_file, cv_client):
+def AnalyzeImage(image_file, image_data, cv_client):
     print('\nAnalyzing', image_file)
 
     # Specify features to be retrieved (PEOPLE)
+    results = cv_client.analyze(image_data, [VisualFeatures.PEOPLE])
 
+    if results.People is not None:
+        print("\nPeople in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_file)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
 
     # Get image analysis
+        for detected_people in results.People.list:
+            if (detected_people.confidence > 0.5):
+                r = detected_people.bounding_box
+                bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+                draw.rectangle(bounding_box, outline=color, width=3)
+
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'people.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
+
 
 
 if __name__ == "__main__":
