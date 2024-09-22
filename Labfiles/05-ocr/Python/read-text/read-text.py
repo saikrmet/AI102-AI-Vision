f09633fbf4ac34c1df4348fa5e117cbdc9f5a899
saikrmet <saikrmet@gmail.com>
2024-09-22 13:53:47 -0400

    Finished OCR module

--- Labfiles/05-ocr/Python/read-text/read-text.py
+++ Labfiles/05-ocr/Python/read-text/read-text.py
@@ -5,6 +5,9 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 
 # Import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 
 def main():
@@ -18,6 +21,7 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(endpoint=ai_endpoint, credential=AzureKeyCredential(ai_key))
         
 
         # Menu for text reading functions
@@ -42,6 +46,45 @@ def GetTextRead(image_file):
             image_data = f.read()
 
     # Use Analyze image function to read text in image
+    result = cv_client.analyze(image_data=image_data, visual_features=[VisualFeatures.READ])
+
+    if result.read is not None:
+        print("\nText:")
+
+        # Prepare image for drawing
+        image = Image.open(image_file)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for line in result.read.blocks[0].lines:
+            print(f"\nLine: {line.text}")
+            drawLinePolygon = True
+    
+            r = line.bounding_polygon
+            bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y),(r[3].x, r[3].y))
+
+            print("   Bounding Polygon: {}".format(bounding_polygon))
+
+            for word in line.words:
+                r = word.bounding_polygon
+                bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y),(r[3].x, r[3].y))
+                print(f"    Word: '{word.text}', Bounding Polygon: {bounding_polygon}, Confidence: {word.confidence:.4f}")
+                
+                # Draw word bounding polygon
+                drawLinePolygon = True
+                draw.polygon(bounding_polygon, outline=color, width=3)
+                plt.annotate(word.text,(r[0].x, r[0].y), backgroundcolor=color)
+                
+
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'text.jpg'
+        fig.savefig(outputfile)
+        print('\n  Results saved in', outputfile)
+         
+         
     
     
 
